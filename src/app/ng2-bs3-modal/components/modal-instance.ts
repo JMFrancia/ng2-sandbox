c98ad387c8ed45643e45944131368aa0d9ebbd48
Joe Francia <Jordan.Francia@viacom.com>
2017-01-10 17:02:32 -0500

    Got modal working

--- src/app/ng2-bs3-modal/components/modal-instance.ts
+++ src/app/ng2-bs3-modal/components/modal-instance.ts
@@ -3,7 +3,7 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/observable/fromEvent';
 
-//declare var jQuery: any;
+declare var jQuery: any;
 
 export class ModalInstance {
 
